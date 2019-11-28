92f7e60c34bcf869b2303e33544e50cafb7c89ba
Tristan Elliott <tristanelliott7@gmail.com>
2019-11-28 13:09:41 -0400

    this is the parse method with working items

--- steam/spiders/steamscrapper.py
+++ steam/spiders/steamscrapper.py
@@ -9,9 +9,10 @@ class SteamscrapperSpider(scrapy.Spider):
 	'''- the spider class that will run the spider and logic'''
 	name = 'steamscrapper'
 	allowed_domains = ['store.steampowered.com']
-	start_urls = ["https://store.steampowered.com/games/#p=0&tab=NewReleases","https://store.steampowered.com/games/#p=1&tab=NewReleases"]
+	start_urls = ["https://store.steampowered.com/games/#p=0&tab=NewReleases"]
 
 	def parse(self, response):
             l = ItemLoader(item=Product(),response=response)
-            l.add_xpath('title', '//div[@class="tab_item_name"]')
-            l.add_xpath('price', '//div[@class="discount_final_price"]')
+            l.add_xpath('title', '//div[@class="tab_item_name"]/text()')
+            l.add_xpath('price', '//div[@class="discount_final_price"]/text()')
+            return l.load_item()
